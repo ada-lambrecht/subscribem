d58b243363a6cbacfdbf7210ee1b76b6525743bc
Mike Barnard <michael.d.barnard@gmail.com>
2013-06-09 09:29:34 -0400

    Revert "added factory girl"
    This reverts commit 3d239f80716b8a712d67908d28b68ecf6d359d51.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  mount Subscribem::Engine => "/"
+  mount Subscribem::Engine => "/subscribem"
 end
--- spec/support/factories/user_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :user, :class => Subscribem::User do
-    sequence(:email) { |n| "text#{n}@example.com" }
-    password "password"
-  end
-end
