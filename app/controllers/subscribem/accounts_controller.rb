9b5fe728ca337b852ca7bcb0ed37165469e7fda8
Mike Barnard <michael.d.barnard@gmail.com>
2013-06-09 06:42:08 -0400

    p14 added account model

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -2,5 +2,8 @@ require_dependency "subscribem/application_controller"
 
 module Subscribem
   class AccountsController < ApplicationController
+    def new
+      @account = Subscribem::Account.new
+    end
   end
 end
