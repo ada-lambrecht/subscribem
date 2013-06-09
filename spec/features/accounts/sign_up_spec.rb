02bdfc51ec9d7418c474c31e760996590222ebdc
Mike Barnard <michael.d.barnard@gmail.com>
2013-06-09 10:04:25 -0400

    p29 added subdomain uniqueness check
    doesnt seem to be working, getting www.subdomain.example.com instead of
    subdomain.example.com

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,12 +4,29 @@ feature 'Accounts' do
     visit subscribem.root_url
     click_link 'Account Sign Up'
     fill_in 'Name', :with => "Test"
+    fill_in 'Subdomain', :with => "test"
     fill_in 'Email', :with => "subscribem@example.com"
     password_field_id = "account_owner_attributes_password"
     fill_in password_field_id, :with => "password"
     fill_in 'Password confirmation', :with => "password"
     click_button 'Create Account'
-    success_message = 'Signed in as subscribem@example.com'
+    success_message = 'Your account has been successfully created.'
     page.should have_content(success_message)
   end
+  
+  scenario "Ensure subdomain uniqueness" do
+    Subscribem::Account.create!(:subdomain => "test", :name => "Test")
+    visit subscribem.root_path
+    click_link 'Account Sign Up'
+    fill_in 'Name', :with => "Test"
+    fill_in 'Subdomain', :with => "test"
+    fill_in 'Email', :with => "subscribem@example.com"
+    fill_in 'Password', :with => "password"
+    fill_in 'Password confirmation', :with=> "password"
+    click_button "Create Account"
+    page.current_url.should == "http://example.com/subscribem/accounts"
+    page.should have_content("Sorry, your account could not be created.")
+    page.should have_content("Subdomain has already been taken")
+  end
+  
 end
